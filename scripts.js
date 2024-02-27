e1add5cff076f0b24c5deb0da5da6ee30f0c80b0
fugbot <18075133+fugbot@users.noreply.github.com>
2024-02-27 12:55:41 +0000

    Add rainbow functionality and default grid

--- scripts.js
+++ scripts.js
@@ -1,8 +1,11 @@
 const grid = document.querySelector(".grid");
 const btn = document.querySelector("button");
+const rainbow = document.querySelector("#rainbow");
 
 btn.addEventListener("click", changeGridSize);
+rainbow.addEventListener("click", changetoRainbow);
 
+createGrid(16);
 function createGrid(size, eventFunction) {
     let dim = 768/size;
     for(let i = 0; i < size; i++) {
@@ -13,23 +16,11 @@ function createGrid(size, eventFunction) {
             cell.classList.add('cell');
             cell.style.height = dim + 'px';
             cell.style.width = dim + 'px';
-            //cell.addEventListener('mouseover', eventFunction);
+            //cell.addEventListener('mouseover', changeColour);
             row.appendChild(cell);
         }
         grid.appendChild(row);
     }
-}
-
-
-
-
-function changeGridSize(){
-    const size = prompt("Please enter number of grid size (Max 100): ");
-    if(size<1 || size > 100){
-        return null;
-    }
-    removeGrid();
-    createGrid(size);
 
     const cells = document.querySelectorAll(".cell");
     cells.forEach((cell) => {
@@ -43,8 +34,45 @@ function changeGridSize(){
     })
 }
 
+
+
+
+function changeGridSize(){
+    const size = prompt("Please enter number of grid size (Max 100): ");
+    if(size<1 || size > 100){
+        return null;
+    }
+    removeGrid();
+    createGrid(size);
+
+}
+
+function changetoRainbow(){
+    
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach((cell) => {
+        cell.addEventListener("mouseover", () => {
+            cell.style.backgroundColor = rainbowGenerator();
+        });
+
+        cell.addEventListener("mouseout", () => {
+            cell.style.backgroundColor = rainbowGenerator();
+        })
+    })
+    
+     
+}
+
 function removeGrid(){
     grid.innerHTML="";
+}
+
+function rainbowGenerator(){
+    const red = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
+
+    return "rgb( " + red + ", " + green + ", " + blue + ")";
 }
 
 
